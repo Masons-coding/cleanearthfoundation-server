a8db1db5e69f5725752feac35553f05d7193804d
Mason Clarke <maclarkegdci@gmail.com>
2023-01-26 19:03:51 -0500

    Changed database host
    Changed host for database

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const {
   DB_LOCAL_USER,
   DB_LOCAL_PASSWORD,
-  DB_LOCAL_DBNAME
+  DB_LOCAL_DBNAME,
+  DB_LOCAL_HOST
 } = process.env;
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -11,7 +12,7 @@ const {
 module.exports = {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
+    host: DB_LOCAL_HOST,
     user: DB_LOCAL_USER,
     password: DB_LOCAL_PASSWORD,
     database: DB_LOCAL_DBNAME,
