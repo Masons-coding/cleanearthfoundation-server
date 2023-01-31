3b9e624d820149fed046954c97b440c3b1e063c3
Mason Clarke <maclarkegdci@gmail.com>
2023-01-30 20:32:22 -0500

    Added env variables for DB
    Added env variables for DB

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 
 const {
-  DB_LOCAL_USER,
-  DB_LOCAL_PASSWORD,
-  DB_LOCAL_DBNAME
+  DB_CONNECTION
 } = process.env;
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -11,10 +9,6 @@ const {
 module.exports = {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
-    user: DB_LOCAL_USER,
-    password: DB_LOCAL_PASSWORD,
-    database: DB_LOCAL_DBNAME,
-    charset: "utf8",
+    DB_CONNECTION
   }
 };
