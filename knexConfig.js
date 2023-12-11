e203cfa3ae50a75c72ff6e1eb8aa87b45c570f9a
Mason Clarke <maclarkegdci@gmail.com>
2023-12-10 23:16:31 -0500

    Changed back to local host DB and server
    Changed back to local host DB and server so I can re-do hosting.

--- knexConfig.js
+++ knexConfig.js
@@ -1,3 +1,15 @@
+
+/** knex config for hosted services */ 
+/**------------------------------------- */
+/** 
 const env = process.env.NODE_ENV || 'development';
 const knex = require('knex')(require('./knexfile')[env]);
+module.exports = knex;
+*/
+
+
+/** knex.js for local host */ 
+/**------------------------------------- */ 
+const knex = require('knex')(require('./knexfile'));
+
 module.exports = knex;
